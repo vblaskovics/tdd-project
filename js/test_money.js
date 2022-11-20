f3da36a33979b1207ae4ae31e5cedb13745da2ba
vblaskovics <viktor.blaskovics@gmail.com>
2022-11-20 13:13:47 +0100

    feat: addition feature for Moneys in the same currency done

--- js/test_money.js
+++ js/test_money.js
@@ -15,19 +15,38 @@ class Money {
   }
 }
 
+class Portfolio {
+
+  constructor() {
+    this.moneys = [];
+  }
+
+  add(...moneys) {
+    this.moneys = this.moneys.concat(moneys);
+  }
+
+  evaluate(currency) {
+    let total = this.moneys.reduce( (sum, money ) => {
+      return sum + money.amount;
+    }, 0);
+    return new Money(total, currency);
+  }
+}
+
 let fiveDollars = new Money(5, "USD");
 let tenDollars = new Money(10, "USD");
 assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
-
-
-// Chapter 2.
 
 let tenEuros = new Money(10, "EUR");
 let twentyEuros = new Money(20, "EUR");
 assert.deepStrictEqual(tenEuros.times(2), twentyEuros);
 
-
 let originalMoney = new Money(4002, "KRW");
 let actualMoneyAfterDivision = originalMoney.divide(4);
 let expectedValueAfterDivision = new Money(1000.5, "KRW");
 assert.deepStrictEqual(actualMoneyAfterDivision, expectedValueAfterDivision);
+
+let fifteenDollars = new Money(15, "USD");
+let portfolio = new Portfolio();
+portfolio.add(fiveDollars, tenDollars);
+assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
